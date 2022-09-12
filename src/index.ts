a41b1b8020f4b60b4c30ccb08493cace15bb08df
mrsaifullah52 <mrsaifullah52@gmail.com>
2022-09-12 15:22:52 +0500

    token mint created successfully

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
-import { initializeKeypair } from './initializeKeypair'
-import * as web3 from '@solana/web3.js'
+import { initializeKeypair } from "./initializeKeypair";
+import * as web3 from "@solana/web3.js";
+import * as token from "@solana/spl-token";
 
 async function main() {
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
-    const signer = await initializeKeypair(connection)
-    
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  const user = await initializeKeypair(connection);
+
+  const mint = await createNewMint(
+    connection,
+    user,
+    user.publicKey,
+    user.publicKey,
+    2
+  );
 }
 
-main().then(() => {
-    console.log('Finished successfully')
-    process.exit(0)
-}).catch(error => {
-    console.log(error)
-    process.exit(1)
-})
+main()
+  .then(() => {
+    console.log("Finished successfully");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+// creating New Token Mint
+async function createNewMint(
+  connection: web3.Connection,
+  payer: web3.Keypair,
+  mintAuthority: web3.PublicKey,
+  freezeAuthority: web3.PublicKey,
+  decimals: number
+): Promise<web3.PublicKey> {
+  const tokenMint = await token.createMint(
+    connection,
+    payer,
+    mintAuthority,
+    freezeAuthority,
+    decimals
+  );
+
+  console.log(
+    `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
+  );
+
+  return tokenMint;
+}
